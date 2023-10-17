b14f62895206347580713a91e2f5bad12cde5b6c
moosa <moosaabdullahi45@gmail.com>
2023-10-17 11:50:34 +0200

    add excel sheet

--- src/components/annoucement/Accouncement.jsx
+++ src/components/annoucement/Accouncement.jsx
@@ -1,38 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
 
+
 const Announcement = () => {
-  const items = [
-    { crop: "maize", price: "$2.6", percentage: "10%" },
-    { crop: "banana", price: "$2.6", percentage: "10%" },
-    { crop: "mango", price: "$2.6", percentage: "10%" },
-    { crop: "juice-lemon", price: "$2.6", percentage: "10%" },
-    { crop: "orange", price: "$2.6", percentage: "10%" },
-    { crop: "orange", price: "$2.6", percentage: "10%" },
-    { crop: "orange", price: "$2.6", percentage: "10%" },
-    { crop: "orange", price: "$2.6", percentage: "10%" },
-    { crop: "orange", price: "$2.6", percentage: "10%" },
-    { crop: "orange", price: "$2.6", percentage: "10%" },
-    { crop: "orange", price: "$2.6", percentage: "10%" },
-    { crop: "orange", price: "$2.6", percentage: "10%" },
-    { crop: "orange", price: "$2.6", percentage: "10%" },
-    { crop: "orange", price: "$2.6", percentage: "10%" },
-  ];
+  const [loading, setLoading] = useState(true);
+  const [itemData, setItemData] = useState([]);
+
+  const getData = async () => {
+    try{
+      const res = await fetch(
+        "https://sheetdb.io/api/v1/yvnmlerq0igpo"
+      )
+      const data = await(res.json())
+      setItemData(data)
+      setLoading(false)
+    }catch (error){
+      console.log(error);
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+ 
   return (
     <div className=" bg-gray-100 flex justify-between items-center py-1 px-0 font-[.8rem]">
       <div>
-        <button className="bg-[#42878b] hover:bg-[#76e6ea] text-white py-2 px-4 rounded whitespace-nowrap">Live data</button>
+        <button
+          onClick={() => getData()}
+          className="bg-[#42878b] hover:bg-[#76e6ea] text-white py-2 px-4 rounded whitespace-nowrap"
+        >
+          Live data
+        </button>
       </div>
-      <Marquee speed={80} pauseOnHover gradient={false}>
-        {items?.map((item, index) => (
-          <AnnouncementText
-            crop={item.crop}
-            price={item.price}
-            percentage={item.percentage}
-            key={index}
-          />
-        ))}
-      </Marquee>
+      {loading ? (
+        <div>Loading data...</div>
+      ) : (
+        <Marquee speed={80} pauseOnHover gradient={false}>
+          {itemData &&
+            itemData.map((item, index) => (
+              <AnnouncementText
+                crop={item.Commodity}
+                price={item.Price}
+                percentage={item.Percentage}
+                // date={item.date}
+                key={index}
+              />
+            ))}
+        </Marquee>
+      )}
+      
     </div>
   );
 };
@@ -40,9 +59,13 @@ const Announcement = () => {
 const AnnouncementText = ({ crop, price, percentage }) => {
   return (
     <div className="flex items-center mr-8">
-      <span className="not-last-child:mr-4 font-serif last-child:mr-0 mr-4">{crop}</span>
-      <span className="not-last-child:mr-4 last-child:mr-1 mr-4">{price}</span>
-      <span className="not-last-child:mr-4 font-serif last-child:mr-1 mr-4">{percentage}</span>
+      <span className="mr-4 font-serif not-last-child:mr-4 last-child:mr-0">
+        {crop}
+      </span>
+      <span className="mr-4 not-last-child:mr-4 last-child:mr-1">{price}</span>
+      <span className="mr-4 font-serif not-last-child:mr-4 last-child:mr-1">
+        {percentage}
+      </span>
     </div>
   );
 };
